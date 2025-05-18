1b9fe9d21693c97c4e87f42dc379a53e7e5a1e15
salaheddine.tobok <salaheddine.tobok@eadn.dz>
2025-05-18 14:01:38 +0100

    fC

--- src/main/java/com/webcodein/lrpsp/ReactiveTutorial.java
+++ src/main/java/com/webcodein/lrpsp/ReactiveTutorial.java
@@ -1,4 +1,35 @@
 package com.webcodein.lrpsp;
 
+import org.springframework.boot.SpringApplication;
+import reactor.core.publisher.Mono;
+
 public class ReactiveTutorial {
+
+    // 01 : First example
+    private Mono<String> toMono(){
+        return Mono.just("Hello World");
+    }
+
+    // 01 : Second example
+    private Mono<String> toMonoWithLog(){
+        return Mono.just("Hello World")
+                .log();
+    }
+
+    public static void main(String[] args) {
+        ReactiveTutorial reactiveTutorial = new ReactiveTutorial();
+
+        // 01 : First example
+        // 01 : First example | It won't display anything
+        reactiveTutorial.toMono();
+        // 01 : First example | It won't display anything
+        reactiveTutorial.toMono().subscribe();
+        // 01 : First example | It'll print " Hello World "
+        reactiveTutorial.toMono().subscribe(System.out::println);
+
+        // 02 : Second example
+        // 02 : Second example | It'll print " Hello World " with log
+        reactiveTutorial.toMonoWithLog().subscribe(System.out::println);
+    }
+
 }
