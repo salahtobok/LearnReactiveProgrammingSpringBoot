0ff34e59930fa3857f725bf9a842df81060a9956
salaheddine.tobok <salaheddine.tobok@eadn.dz>
2025-05-18 12:36:15 +0100

    fC

---
+++ src/main/java/com/webcodein/lrpsp/LearnReactiveProgrammingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.webcodein.lrpsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnReactiveProgrammingSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnReactiveProgrammingSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webcodein/lrpsp/LearnReactiveProgrammingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webcodein.lrpsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnReactiveProgrammingSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
