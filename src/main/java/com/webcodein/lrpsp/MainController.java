eaaf03eb7c1a92d03f15153850521032aa8e0f3e
salaheddine.tobok <salaheddine.tobok@eadn.dz>
2025-05-24 23:18:18 +0100

    fC

---
+++ src/main/java/com/webcodein/lrpsp/MainController.java
@@ -0,0 +1,17 @@
+package com.webcodein.lrpsp;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import reactor.core.publisher.Mono;
+
+@Controller
+public class MainController {
+
+
+    @GetMapping("/")
+    public Mono<String> handleMain(){
+        return Mono.just("home");
+    }
+
+}
