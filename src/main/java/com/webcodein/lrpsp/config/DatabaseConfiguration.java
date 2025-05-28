9e1e0727269553cf20f3793b8ff9d84da9df1153
salaheddine.tobok <salaheddine.tobok@eadn.dz>
2025-05-28 19:20:49 +0100

    Project refactoring ...

--- src/main/java/com/webcodein/lrpsp/config/DatabaseConfiguration.java
+++ src/main/java/com/webcodein/lrpsp/config/DatabaseConfiguration.java
@@ -1,48 +1,48 @@
-package com.webcodein.lrpsp.config;
-
-import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-
-/**
- * Configuration class for setting up reactive MongoDB components.
- * Extends {@link AbstractReactiveMongoConfiguration} to provide MongoDB connection settings.
- */
-@EnableReactiveMongoRepositories  // Enables scanning for reactive MongoDB repositories
+//package com.webcodein.lrpsp.config;
+//
+//import com.mongodb.reactivestreams.client.MongoClient;
+//import com.mongodb.reactivestreams.client.MongoClients;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+//import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+//import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+//
+///**
+// * Configuration class for setting up reactive MongoDB components.
+// * Extends {@link AbstractReactiveMongoConfiguration} to provide MongoDB connection settings.
+// */
+//@EnableReactiveMongoRepositories  // Enables scanning for reactive MongoDB repositories
 //@Configuration                     // Marks this class as a Spring configuration class
-public class DatabaseConfiguration extends AbstractReactiveMongoConfiguration {
-
-    /**
-     * Defines a {@link MongoClient} bean using the default connection string (localhost).
-     *
-     * @return a reactive MongoDB client
-     */
-    @Bean
-    public MongoClient mongoClient() {
-        return MongoClients.create(); // Uses default settings: mongodb://localhost:27017
-    }
-
-    /**
-     * Provides the name of the MongoDB database to connect to.
-     *
-     * @return the name of the database
-     */
-    @Override
-    protected String getDatabaseName() {
-        return "webcodein-learn-reactive-programming-db";
-    }
-
-    /**
-     * Defines the {@link ReactiveMongoTemplate} bean for reactive MongoDB operations.
-     *
-     * @return a configured instance of ReactiveMongoTemplate
-     */
-    @Bean
-    public ReactiveMongoTemplate reactiveMongoTemplate() {
-        return new ReactiveMongoTemplate(mongoClient(), getDatabaseName());
-    }
-}
+//public class DatabaseConfiguration extends AbstractReactiveMongoConfiguration {
+//
+//    /**
+//     * Defines a {@link MongoClient} bean using the default connection string (localhost).
+//     *
+//     * @return a reactive MongoDB client
+//     */
+//    @Bean
+//    public MongoClient mongoClient() {
+//        return MongoClients.create(); // Uses default settings: mongodb://localhost:27017
+//    }
+//
+//    /**
+//     * Provides the name of the MongoDB database to connect to.
+//     *
+//     * @return the name of the database
+//     */
+//    @Override
+//    protected String getDatabaseName() {
+//        return "webcodein-learn-reactive-programming-db";
+//    }
+//
+//    /**
+//     * Defines the {@link ReactiveMongoTemplate} bean for reactive MongoDB operations.
+//     *
+//     * @return a configured instance of ReactiveMongoTemplate
+//     */
+//    @Bean
+//    public ReactiveMongoTemplate reactiveMongoTemplate() {
+//        return new ReactiveMongoTemplate(mongoClient(), getDatabaseName());
+//    }
+//}
